bf45a9cbd58dcbff59a04bc0efab1adedaa1852b
Rajath M R <rajathmr2000@gmail.com>
2022-06-04 17:21:56 +0530

    Project Setup

--- truffle.js
+++ truffle.js
@@ -1,10 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "because harbor connect between before mutual ankle grunt spice hour blood approve"
+
 module.exports = {
   networks: {
+
+    //local block chain
     development: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
-    }
+    },
+
+    //server test net bmlocn chain
+      ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/236c5349844d40e1a1e9ea20eb6d7186`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   solc: {
     // Turns on the Solidity optimizer. For development the optimizer's
